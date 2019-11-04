d371621637e1d93e67774ba9055c86870ee03289
Anjan Kumar <41472950+anjan111@users.noreply.github.com>
2019-11-04 20:47:18 +0530

    Add files via upload

---
+++ sample.py
@@ -0,0 +1,14 @@
+def factorial(num = None):
+   
+        if(num < 0):
+            return None
+        else:
+            fact = 1
+            for var in range(2,num+1):
+                fact = fact*var
+            else:
+                return var
+
+   
+def power(a=None,b = None):
+    return a**b
